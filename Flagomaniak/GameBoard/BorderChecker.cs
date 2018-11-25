5cd7c5c66367a903f74ee29f94d4646ab5883e38
alimuza <44576469+alimuza@users.noreply.github.com>
2018-11-25 20:41:16 +0100

    Add files via upload

---
+++ Flagomaniak/GameBoard/BorderChecker.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Flagomaniak.GameBoard
+{
+    /// <summary>
+    /// Klasa zajmuje się sprawdzaniem, czy punkt znajduje się w granicach danego kształtu. Dostosowuje wymiary kształtu do wymiarów ekranu.
+    /// </summary>
+    class BorderChecker
+    {
+        /// <summary>
+        /// Funkcja sprawdza, czy punkt na ekranie znajduje się w granicach danego kraju.
+        /// </summary>
+        /// <param name="polygon">Lista z punktami wyznaczającymi granice kształtu.</param>
+        /// <param name="width">Aktualna szerokość wyświetlanej mapy.</param>
+        /// <param name="height">Aktualna wysokość wyświetlanej mapy.</param>
+        /// <param name="p">Punkt do sprawdzenia.</param>
+        /// <returns></returns>
+        public bool Check(List<Point> polygon, double width, double height, Point p)
+        {
+            List<Point> newPolygon = ScaleToScreen(polygon, width, height);
+
+            if (IsPointInPolygon(newPolygon, p))
+               return true;
+        
+            return false;
+        }
+
+        /// <summary>
+        /// Funkcja sprawdza, czy dany punkt znajduję się w środku kształtu.
+        /// </summary>
+        /// <param name="polygon">Lista zawierająca punkty z granicami kształtu.</param>
+        /// <param name="point">Współrzędne punktu.</param>
+        /// <returns>Wynik typy bool wskazujący czy punkt znajduje się w granicach kształtu na mapie.</returns>
+        private bool IsPointInPolygon(List<Point> polygon, Point point)
+        {
+            bool result = false;
+            int j = polygon.Count() - 1;
+            for (int i = 0; i < polygon.Count(); i++)
+            {
+                if (polygon[i].Y < point.Y && polygon[j].Y >= point.Y || polygon[j].Y < point.Y && polygon[i].Y >= point.Y)
+                {
+                    if (polygon[i].X + (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) * (polygon[j].X - polygon[i].X) < point.X)
+                    {
+                        result = !result;
+                    }
+                }
+                j = i;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Przeskalowanie wymiarów kraju w zależności od rozmiarów ekranu.
+        /// </summary>
+        /// <remarks>
+        /// Granice kraju zostały wyznaczone w postaci ścieżki na obrazie w rozmiarze 2000x13010.
+        /// Obraz będzie miał inne wymiary w zależności od wielkości ekranu/ rozmiaru okna, potrzebne jest przeskalowanie tej ścieżki w zależności od aktualnego rozmiaru wyświetlanej mapy.
+        /// </remarks>
+        /// <param name="polygon">Lista punktów z granicami kraju.</param>
+        /// <param name="width">Aktualna szerokość mapy na ekranie.</param>
+        /// <param name="height">Aktualna wysokość mapy na ekranie.</param>
+        /// <returns>Punkty określające granice kraju dostosowane do aktualnych rozmiarów ekranu.</returns>
+        private List<Point> ScaleToScreen(List<Point> polygon, double width, double height)
+        {
+            // 2000 X 1310 - prawdziwe wymiary wyświetlanej mapy
+            double x = width / 2000;
+            double y = height / 1310;
+
+            List<Point> newPolygon = new List<Point>();
+            foreach (var point in polygon)
+            {
+                newPolygon.Add(new Point(point.X * x, point.Y * y));
+            }
+            return newPolygon;
+        }
+    }
+}
--- Flagomaniak/GameBoard/Game.cs
+++ Flagomaniak/GameBoard/Game.cs
@@ -1,22 +1,32 @@
 ﻿using Flagomaniak.Continents;
+using Flagomaniak.GameUsers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Flagomaniak.GameBoard
 {
     /// <summary>
-    /// Klasa opisyjąca zahcowanie gry
+    /// Klasa opisująca logikę gry.
     /// </summary>
-    class Game
+    /// <remarks>
+    /// Klasa implementuje interfejs INotifyPropertyChanged, dzięki temu dane wyświetlane w oknie gry są uaktualniane.
+    /// </remarks>
+    class Game : INotifyPropertyChanged
     {
         /// <summary>
-        /// Klasa przechowywująca wszystkie kontynety w grze.
+        /// Zmienna zawierająca wszystkie kontynenty w grze.
         /// </summary>
         private ContinentCollection _continents;
+
+        /// <summary>
+        /// Klasa zajmująca sprawdzaniem, czy dany punkt leży wewnątrz granic kraju.
+        /// </summary>
+        private BorderChecker _borderChecker;
 
         /// <summary>
         /// Aktualny kontynent w grze.
@@ -27,46 +37,106 @@ namespace Flagomaniak.GameBoard
         /// Zmienna typu Random do losowania liczb.
         /// </summary>
         private Random _random;
+
         /// <summary>
-        /// Zmienna przechowywująca nazwy krajów aktualnego kontynetu w grze.
+        /// Zmienna zawierająca nazwy krajów aktualnego kontynentu w grze.
         /// </summary>
         private ObservableCollection<string> _names;
+
         /// <summary>
-        /// Zmienna przechowywująca wszystkie flagi aktualnego kontynentu w grze.
+        /// Zmienna zawierająca ścieżki do plików z flagami.
         /// </summary>
         private ObservableCollection<string> _flags;
 
+        /// <summary>
+        /// Klasa obsługująca historię użytkowników w grze.
+        /// </summary>
+        private Users _userCollection;
 
-        public List<Continent> Continents => _continents.Continents;
+        /// <summary>
+        /// Przechowuje aktualną punktację użytkownika.
+        /// </summary>
+        private int _points = 0;
 
-        //wszytskie publiczne zmienne do interfejsu => powinny inplementować notify propery change!!!
+        /// <summary>
+        /// Imię aktualnie grającego użytkownika.
+        /// </summary>
+        private string _userName = "";
+
+        /// <summary>
+        /// Przechowuje dane o prawidłowych i złych odpowiedziach dla wszystkich krajów.
+        /// </summary>
+        private Dictionary<string, bool> _answers;
+
+        /// <value>Zwraca imię aktualnego gracza.</value>
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        /// <value>Zwraca informację o poprawnych i złych odpowiedziach.</value>
+        public Dictionary<string, bool> Answers
+        {
+            get { return _answers; }
+            set
+            {
+                _answers = value;
+                OnPropertyChanged("Answers");
+            }
+        }
+
+        /// <value>Zwraca nazwy wszystkich krajów.</value>
         public ObservableCollection<string> Names => _names;
+
+        /// <value>Zwraca ścieżki dostępu do wszystkich flag.</value>
         public ObservableCollection<string> Flags => _flags;
 
+        /// <value>Zwraca nazwę kontynentu.</value>
         public string ContinentName
         {
            get { return _currentContinent.Name; }
-           set { } 
+           set { OnPropertyChanged("ContinentName"); }
         }
 
+        /// <value>Zwraca ścieżkę dostępu do pliku z mapą kontynentu.</value>
         public string ContinentMap
         {
             get { return _currentContinent.MapUri; }
-            set { }
+            set { OnPropertyChanged("ContinentMap"); }
         }
 
+        /// <value>Zwraca aktualną punktację uzytkownika.</value>
+        public int Points
+        {
+            get { return _points; }
+            set
+            {
+                _points = value;
+                OnPropertyChanged("Points");
+            }
+        }
 
-
+        /// <summary>
+        /// Konstruktor klasy. 
+        /// </summary>
         public Game()
         {
+            _answers = new Dictionary<string, bool>();
             _continents = new ContinentCollection();
             _names = new ObservableCollection<string>();
             _flags = new ObservableCollection<string>();
             _random = new Random();
+            _borderChecker = new BorderChecker();
+            _userCollection = new Users(@"../../../Usres.xml");
         }
 
         /// <summary>
-        /// Losuje kontynet spośród zbioru wszystkich kontynetów w grze.
+        /// Losuje kontynent spośród zbioru wszystkich kontynentów w grze.
         /// </summary>
         private void DrawContinent()
         {
@@ -79,7 +149,7 @@ namespace Flagomaniak.GameBoard
         }
 
         /// <summary>
-        /// Ustwaia nazwy państw oraz flagi do wyswietlenia w graficznym interfejsie uzytkownika.
+        /// Ustawia nazwy państw oraz flagi do wyświetlenia w graficznym interfejsie użytkownika.
         /// </summary>
         private void SetNamesAndFlags()
         {
@@ -90,19 +160,117 @@ namespace Flagomaniak.GameBoard
                 _names.Add(c.Name);
                 _flags.Add(c.FlagUri);
             }
+            ShuffleList(ref _names);
+            ShuffleList(ref _flags);
         }
 
         /// <summary>
-        /// Ustawianie nowego kontynentu.
+        /// Funkcja miesza listy zawierające nazwy państw oraz flagi państw, aby nie pojawiały się zawsze w tej samej kolejności.
         /// </summary>
-        public void NewContinent()
+        /// <param name="list">Lista do wymieszania.</param>
+        private void ShuffleList(ref ObservableCollection<string> list)
+        {
+            int count = list.Count;
+            int index = 0;
+            for(int i =0; i< count; i++)
+            {
+                index = _random.Next(0, count);
+                list.Add(list[index]);
+                list.RemoveAt(index);
+            }
+        }
+
+        /// <summary>
+        /// Ustawianie nowej gry.
+        /// </summary>
+        public void NewGame()
         {
             DrawContinent();
             SetNamesAndFlags();
+            ChooseAPlayer();
         }
 
-        
+        /// <summary>
+        /// Sprawdza, czy flaga bądź nazwa państwa zostały upuszczone w odpowiednim miejscu na mapie i zapisuje wyniki do odpowiedniej zmiennej.
+        /// </summary>
+        /// <param name="name">Nazwa kraju.</param>
+        /// <param name="point">Punkt na mapie, w którym upuszono flagę bądź nazwę kraju.</param>
+        /// <param name="width">Aktualna szerokość mapy.</param>
+        /// <param name="height">Aktualna wysokość mapy.</param>
+        public void CheckAnswer(string name, Point point, double width, double height)
+        {
+            Country country = _currentContinent.Countries.Find(c => c.Name == name);
+            List<Point> countryBorders = country.Polygon;
+            bool result;
 
+            if(_borderChecker.Check(countryBorders, width, height, point))
+                result = true;
+            else
+                result = false;
+           
+            if (!Answers.ContainsKey(name))
+                Answers.Add(name, result);
+            else
+                Answers[name] = result;
+        }
 
+        /// <summary>
+        /// Obliczanie ostatecznej punktacji na podstawie odpowiedzi oraz oznaczenie poprawnych odpowiedzi na zielono i złych odpowiedzi na czerwono.
+        /// </summary>
+        /// <param name="map">Lista flag oraz nazw państw na mapie.</param>
+        public void GetFinalScore(UIElementCollection map)
+        {
+            foreach (var p in Answers)
+            {
+                if (p.Value == true)
+                    Points += 25;
+                else
+                    Points -= 10;
+
+                foreach (var elem in map)
+                {
+                    if (elem is Label)
+                    {
+                        Label label = elem as Label;
+                        if ((string)label.Content == p.Key && p.Value == true)
+                            label.Background = new SolidColorBrush(Colors.Green);
+                        else if((string)label.Content == p.Key && p.Value == false)
+                            label.Background = new SolidColorBrush(Colors.Red);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Funkcja służąca do wyboru użytkownika w grze. Wyświetla nowe okno z polem wyboru użytkownika.
+        /// </summary>
+        private void ChooseAPlayer()
+        {
+            PlayerWindow choosePlayer = new PlayerWindow(_userCollection);
+            choosePlayer.ShowDialog();
+            UserName = choosePlayer.User;
+        }
+
+        /// <summary>
+        /// Zapisanie wyniku użytkownika do historii.
+        /// </summary>
+        public void SaveGame()
+        {
+            _userCollection.SavePlayer(_userName, _points);
+        }
+
+        /// <summary>
+        /// Zdarzenie zmiany wartości właściwości klasy.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Funkcja wywołuje się, gdy zmieni się wartość właściwości.
+        /// </summary>
+        /// <param name="name">Nazwa właściwości, której wartość uległa zmianie.</param>
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
